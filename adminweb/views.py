dd57602b33b3bbbc96dff2d802723d450ae85ff5
artdidit2506 <artdidit2506@gmail.com>
2022-06-16 18:05:32 +0700

    update be dan adminweb

--- adminweb/views.py
+++ adminweb/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def adminweb(request):
+    return render(request, 'adminweb/adminweb.html')
--- base/views.py
+++ base/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def basehome(request):
+    return render(request, 'base/basehome.html')
