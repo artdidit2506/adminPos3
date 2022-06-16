8ece281f8a0aecded7c1257f5d8b23d9b5d9cae3
artdidit2506 <artdidit2506@gmail.com>
2022-06-16 18:57:49 +0700

    update url base dan adminweb

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('base/', include('base.urls')),
+    path('', include('base.urls')),
     path('adminweb/', include('adminweb.urls')),
 ]
